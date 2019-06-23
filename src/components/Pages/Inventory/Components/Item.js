6179df9843182e01d75280166f319ac09d14ad61
unknown <yarinhala123@gmail.com>
2019-06-23 21:30:00 +0300

    fix hmoe bug

--- src/components/Pages/Inventory/Components/Item.js
+++ src/components/Pages/Inventory/Components/Item.js
@@ -13,27 +13,37 @@ class Item extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                {
-                    this.props.current_display_items.map((item, i) => {
-                        return (
-                            <span className="inventory_item" key={i}>
-                                <img alt="pic" className="item_img" src={item.picpath} />
-                                <a
-                                    className="item_title"
-                                    href={'../item/' + item._id}
-                                    key={i}
-                                >
-                                    {item.title}
-                                </a>
-                                <p className="item_description">{item.desc}</p>
-                            </span>
-                        )
-                    })
-                }
-            </div>
-        );
+        console.log(this.props.current_display_items)
+        if(this.props.current_display_items.length < 0)
+        {       
+            return (
+                <div>
+                    {
+                        this.props.current_display_items.map((item, i) => {
+                            return (
+                                <span className="inventory_item" key={i}>
+                                    <img alt="pic" className="item_img" src={item.picpath} />
+                                    <a
+                                        className="item_title"
+                                        href={'../item/' + item._id}
+                                        key={i}
+                                    >
+                                        {item.title}
+                                    </a>
+                                    <p className="item_description">{item.desc}</p>
+                                </span>
+                            )
+                        })
+                    }
+                </div>
+            );
+        }else{
+            return(
+                <div>
+
+                </div>
+            )
+        }
     }
 }
 export default Item;
